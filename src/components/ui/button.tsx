b07e8d987b10d02b77d716c99bfcab6c22a7d948
hussain <hussainrizvi486@gmail.com>
2025-08-05 10:37:21 +0500

    ts build issues fixed

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,9 @@
-import { cn } from "@utils";
+import { cn } from "../../utils";
 import { cva, type VariantProps } from "class-variance-authority";
-import React, { forwardRef, ButtonHTMLAttributes } from "react";
+import { Loader2 } from "lucide-react";
+import React, { forwardRef } from "react";
 
+import type { ButtonHTMLAttributes } from "react";
 const buttonVariants = cva(
     "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
     {
@@ -75,4 +77,4 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 
 Button.displayName = "Button";
 
-export { Button, buttonVariants };
+export { Button };
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
-import path from "path";
-
-
+import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    tsconfigPaths()
   ],
-  resolve: {
-    alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-    }
-  }
+ 
 })
